f4abe24bd22456a9f8a6567af17a1451aa39cfb3
Daldek <piotr.de.bever@gmail.com>
2021-08-22 10:32:46 +0200

    Fibonacci sequence

---
+++ Fibonacci_sequence.py
@@ -0,0 +1,19 @@
+'''
+Obliczenie kolejnych wartosci ciagu Fibonacciego
+'''
+
+# rekurencja
+def fibonacci_r(n):  # O(2^n)
+    if n <= 1:
+        return n
+    return fibonacci_r(n-1) + fibonacci_r(n-2)
+
+# liniowe
+def fibonacci_l(n):  # O(n)
+    p, d = 0, 1
+    for _ in range(n):
+        p, d = d, p + d
+    return p
+
+print(fibonacci_r(9))
+print(fibonacci_l(10))
